e1d7332bf539d9f68f4cfea9fc18cb76f2667bab
Nanashikung <nata.station@hotmail.com>
2022-10-06 14:57:44 +0700

    finished

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,9 +1,32 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB } from "../../backendLibs/dbLib";
+
 export default function summaryRoute(req, res) {
+  let sumcus = 0,
+    sumadmin = 0,
+    summoney = 0;
   if (req.method === "GET") {
     //check authentication
-    //return res.status(403).json({ ok: false, message: "Permission denied" });
+    const user = checkToken(req);
+    if (!user || !user.isAdmin)
+      return res.status(403).json({ ok: false, message: "Permission denied" });
     //compute DB summary
+
+    const users = readUsersDB();
+    users.map((x) => {
+      if (x.isAdmin === true) sumadmin += 1;
+      if (x.isAdmin === false) {
+        sumcus += 1;
+        summoney += x.money;
+      }
+    });
     //return response
+    return res.json({
+      ok: true,
+      userCount: sumcus,
+      adminCount: sumadmin,
+      totalMoney: summoney,
+    });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
